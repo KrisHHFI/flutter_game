d323d50ff87c1c2f22c345b1d6e57cc9efb81681
KrisHHFI <kristopherpepperuk@gmail.com>
2024-10-04 09:26:59 +0300

    Some tidying

--- lib/widgets/increment_counter.dart
+++ lib/widgets/increment_counter.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class IncrementCounter extends StatefulWidget {
-  final int counter; // Accept counter value as a parameter
+  final int counter;
 
   const IncrementCounter({super.key, required this.counter});
 
   @override
-  _IncrementCounterState createState() => _IncrementCounterState();
+  IncrementCounterState createState() => IncrementCounterState();
 }
 
-class _IncrementCounterState extends State<IncrementCounter> {
+class IncrementCounterState extends State<IncrementCounter> {
   @override
   Widget build(BuildContext context) {
     return Text(
-      '${widget.counter} kg', // Use the counter value from the parent
+      '${widget.counter} kg',
       style: Theme.of(context).textTheme.headlineMedium,
     );
   }
