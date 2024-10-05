71f3f7d77de13ea029264ec5ec3e35226e499af4
KrisHHFI <kristopherpepperuk@gmail.com>
2024-10-05 23:24:02 +0300

    Minor

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 5;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
